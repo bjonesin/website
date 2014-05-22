e8cebd90575ab8e3d7da160819f1c4376c8a8cd4
berkley <bjonesin@gmail.com>
2014-05-21 21:11:45 -0500

    Beginning stage of blog

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+export DEBIAN_FRONTEND=noninteractive
+sudo aptitude -y install build-essential
+sudo aptitude -y install python-dev
+sudo aptitude -y install python-software-properties
+sudo aptitude -y install python-pip
+sudo aptitude -y install nginx
+sudo pip install ipython
+sudo pip install Django==1.5.1
+sudo pip install South
+
